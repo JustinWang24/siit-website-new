00f85061a50637f1b1fb04a331c416792db19ae3
wangyue <18510209803@163.com>
2018-07-17 17:23:06 +1000

    U

--- resources/views/frontend/custom/siit/layouts/frontend/header/general.blade.php
+++ resources/views/frontend/custom/siit/layouts/frontend/header/general.blade.php
@@ -8,7 +8,7 @@
                 @if(empty($siteConfig->logo))
                     {{ str_replace('_',' ',env('APP_NAME','Home')) }}
                 @else
-                    <a id="logo-top" class="" href="{{ url('/') }}" style="margin-top: 11px;margin-left:27px;">
+                    <a id="logo-top" class="" href="{{ url('/') }}" style="margin-top: 19px;margin-left:27px;">
                         {!! \App\Models\Utils\AMP\MediaUtil::NormalImage(asset($siteConfig->logo_dark),'SIIT: a bridge across cultures', '50%', null, 'image') !!}
                     </a>
                 @endif
