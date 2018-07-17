99f722eb77d4b6d02b6601038197656ed6a3a94b
wangyue <18510209803@163.com>
2018-07-17 15:54:30 +1000

    Update title to support cn

--- resources/views/frontend/custom/siit/templates/two_columns_left.blade.php
+++ resources/views/frontend/custom/siit/templates/two_columns_left.blade.php
@@ -47,7 +47,7 @@
 
                 </div>
                 <div class="page-title-wrap">
-                    <h1 class="is-size-1-desktop is-size-1-mobile">{{ $page->title }}</h1>
+                    <h1 class="is-size-1-desktop is-size-1-mobile">{{ app()->getLocale()=='cn'?$page->title_cn:$page->title }}</h1>
                 </div>
                 <div class="content page-content-wrap">
                     {!! $page->rebuildContent() !!}
