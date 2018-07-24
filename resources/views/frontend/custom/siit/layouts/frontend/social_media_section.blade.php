494c892d4653bf2862da2fe0c611eebb801f90b8
wangyue <18510209803@163.com>
2018-07-25 09:01:17 +1000

    u

--- resources/views/frontend/custom/siit/layouts/frontend/social_media_section.blade.php
+++ resources/views/frontend/custom/siit/layouts/frontend/social_media_section.blade.php
@@ -1,34 +1,24 @@
-<section class="section is-paddingless we-social-wrap">
+<section class="section is-paddingless we-social-wrap" style="min-height: 220px;">
     <div class="container">
         <div class="columns is-marginless">
-            <div class="column first-visiting">
-                <h2>WE</h2>
-                <h2>ARE</h2>
-                <h2><span class="super-bold">SOCIAL</span></h2>
-            </div>
-            <div class="column is-half">
-                <div class="box social-msg mt-20">
-                    <p>Duis pretium felis at dui fringilla convallis. Phasellus eleifend sollicitudin</p>
-                    <p class="has-text-right has-text-grey-light">1 day ago</p>
-                </div>
-                <div class="box social-msg">
-                    <p>Duis pretium felis at dui fringilla convallis. Phasellus eleifend sollicitudin</p>
-                    <p class="has-text-right has-text-grey-light">2 days ago</p>
+            <div class="column "></div>
+            <div class="column ">
+                <br>
+                <div class="box facebook-btn mt-20">
+                    <a href="{{ $siteConfig->facebook }}">
+                        <i class="fab fa-facebook-f"></i>&nbsp;FACEBOOK
+                    </a>
                 </div>
             </div>
             <div class="column ">
                 <br>
-                <div class="box facebook-btn mt-20">
-                    <a href="#">
-                        <i class="fab fa-facebook-f"></i>&nbsp;FACEBOOK
-                    </a>
-                </div>
-                <div class="box twitter-btn">
-                    <a href="#">
+                <div class="box twitter-btn mt-20">
+                    <a href="{{ $siteConfig->facebook }}">
                         <i class="fab fa-twitter"></i>&nbsp;FOLLOW
                     </a>
                 </div>
             </div>
+            <div class="column "></div>
         </div>
     </div>
 </section>
