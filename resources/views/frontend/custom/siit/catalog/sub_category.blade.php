dc9654a76b07c7c12e3b0456c8c99b3c03fe3dd8
Justin Wang <justinwang24@yahoo.com.au>
2018-07-31 23:19:04 +1000

    add cn in category

--- resources/views/frontend/custom/siit/catalog/sub_category.blade.php
+++ resources/views/frontend/custom/siit/catalog/sub_category.blade.php
@@ -11,7 +11,7 @@
             <div class="column">
             @foreach($courses as $p)
                 <div class="card mb-20">
-                    <h2 class="is-size-5 ml-20 mt-20">
+                    <h2 class="is-size-5 ml-20 mt-20 pt-20">
                         <a href="{{ url('catalog/product/'.$p->uri) }}">
                             {{ $p->getName() }} - {{ $p->brand }} {{ trans('general.Campus') }}
                         </a>
