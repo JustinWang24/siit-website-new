fb0b3150e2fa9dd82eeb039f8dc0d8144218c1b4
Justin Wang <justinwang24@yahoo.com.au>
2018-07-31 23:11:32 +1000

    add cn in category

--- resources/views/frontend/custom/siit/catalog/sub_category.blade.php
+++ resources/views/frontend/custom/siit/catalog/sub_category.blade.php
@@ -3,7 +3,7 @@
     <div class="container mt-20 mb-20 pl-10 pr-10 categories-wrapper" id="category-view-manager">
         <div class="columns">
             <div class="column">
-                <h1 class="is-size-4 pl-10">{{ $category->getName() }}</h1>
+                <h1 class="is-size-3 pl-10 has-text-centered">{{ $category->getName() }}</h1>
             </div>
         </div>
 
@@ -11,13 +11,11 @@
             <div class="column">
             @foreach($courses as $p)
                 <div class="card mb-20">
-                    <div class="card-title">
-                        <h2 class="is-size-5 m-5">
-                            <a href="{{ url('catalog/product/'.$p->uri) }}">
-                                {{ $p->getName() }} - {{ $p->brand }} {{ trans('general.Campus') }}
-                            </a>
-                        </h2>
-                    </div>
+                    <h2 class="is-size-5 ml-20 mt-20">
+                        <a href="{{ url('catalog/product/'.$p->uri) }}">
+                            {{ $p->getName() }} - {{ $p->brand }} {{ trans('general.Campus') }}
+                        </a>
+                    </h2>
                     <div class="card-content">
                         <div class="content pl-20">
                         {!! $p->getShortDescription() !!}
