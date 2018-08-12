1e59d16d03af7bf7a0db49c3e7dd70588e15fccd
Justin Wang <justinwang24@yahoo.com.au>
2018-08-12 13:06:56 +1000

    import groups

--- resources/views/backend/groups/form.blade.php
+++ resources/views/backend/groups/form.blade.php
@@ -41,7 +41,7 @@
                             <div class="select full-width">
                                 <select class="full-width" name="status">
                                     <option value="{{ \App\Models\Group::STATUS_ACTIVE }}" {{ $group->status===\App\Models\Group::STATUS_ACTIVE ? 'selected' : null }}>上线</option>
-                                    <option value="{{ \App\Models\Group::STATUS_DISABLE }}" {{ $group->status===\App\Models\Group::STATUS_DISABLE ? null : 'selected' }}>暂停</option>
+                                    <option value="{{ \App\Models\Group::STATUS_DISABLE }}" {{ $group->status===\App\Models\Group::STATUS_DISABLE ? 'selected' : null }}>暂停</option>
                                 </select>
                             </div>
                         </div>
