7e8b1467a65b112a1c40a98850dbaf5c409db5a0
Justin Wang <justinwang24@yahoo.com.au>
2018-09-02 14:46:04 +1000

    Update enrolment form page

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,7 +26,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/frontend/customers/login';
 
     /**
      * Create a new controller instance.
