259375ac8cb4fbf210fe5be195bdcf6c22889388
Justin Wang <justinwang24@yahoo.com.au>
2018-09-02 14:58:18 +1000

    Update enrolment form page

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => '密码长度至少为6位',
-    'reset' => '您的登录密码已经重置!',
-    'sent' => '重置密码链接已经发送到您的邮箱!',
-    'token' => '密码重置链接已经过期了',
-    'user' => "您输入的邮箱在系统中不存在",
+    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
 
 ];
