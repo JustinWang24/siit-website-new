bd2b4af01690bce1e3dad37b68dfe9e8d3dcd05a
Justin Wang <justinwang24@yahoo.com.au>
2018-09-09 04:29:30 +1000

    Optimize menu

--- resources/views/frontend/custom/siit/layouts/frontend/header/maga_menu.blade.php
+++ resources/views/frontend/custom/siit/layouts/frontend/header/maga_menu.blade.php
@@ -37,12 +37,12 @@
                     <a class="navbar-link" href="{{ $rootMenu->link_to=='/' ? '/' : $rootMenu->getMenuUrl() }}" title="{{ app()->getLocale()=='cn' && !empty($rootMenu->name_cn) ? $rootMenu->name_cn : $rootMenu->name }}">
                         {{ app()->getLocale()=='cn' && !empty($rootMenu->name_cn) ? $rootMenu->name_cn : $rootMenu->name }}
                     </a>
-                    <ul style="width: 75.5%;">
+                    <ul style="width: 60.5%;margin-left: 15%;">
                         <?php
                         $children = $rootMenu->getSubMenus();
                         ?>
                         <li style="width: 50%;">
-                            <a class="is-size-5-desktop is-link" href="#" title="{{ trans('general.Local_Students') }}">
+                            <a class="is-size-5-desktop" style="color: #3273dc;" href="#" title="{{ trans('general.Local_Students') }}">
                                 {{ trans('general.Local_Students') }}
                             </a>
                             @if(count($children) > 0)
@@ -69,26 +69,41 @@
                         </li>
 
                         <li style="width: 50%;">
-                            <a class="is-size-5-desktop is-link" href="#" title="{{ trans('general.International_Students') }}">
+                            <a class="is-size-5-desktop" style="color: #3273dc;" href="#" title="{{ trans('general.International_Students') }}">
                                 {{ trans('general.International_Students') }}
                             </a>
                             @if(count($children) > 0)
                                 <ul>
                                     @include(_get_frontend_layout_path('frontend.header.courses_submenu'), ['showOnly' => 'Courses for International Students'])
                                     @foreach($children as $sub)
-                                        @php
-                                            $menuUrl = $sub->getMenuUrl();
-                                            $pathwaySubmenu = strpos($menuUrl,'University-Pathway-Collection') !== false;
-                                        @endphp
-                                        <div class="aside">
-                                            <ul class="menu-list">
-                                                <li>
-                                                    <a href="{{ $sub->link_to=='/' ? '/' : $sub->getMenuUrl() }}" title="{{ app()->getLocale()=='cn' && !empty($sub->name_cn) ? $sub->name_cn : $sub->name }}">
-                                                        {{ app()->getLocale()=='cn' && !empty($sub->name_cn) ? $sub->name_cn : $sub->name }}
+                                        @if(count($sub->children)>0)
+                                            <div class="navbar-item has-dropdown is-hoverable">
+                                                <a class="navbar-link" href="#" title="{{ app()->getLocale()=='cn' && !empty($sub->name_cn) ? $sub->name_cn : $sub->name }}" style="border-bottom: none;">
+                                                    {{ app()->getLocale()=='cn' && !empty($sub->name_cn) ? $sub->name_cn : $sub->name }}
+                                                </a>
+                                                <div class="navbar-dropdown is-boxed" style="min-width: 95%;">
+                                                    @foreach($sub->children as $subChild)
+                                                    <a  style="border-bottom: none;" class="navbar-item" href="{{ $subChild->getMenuUrl() }}" title="{{ app()->getLocale()=='cn' && !empty($subChild->name_cn) ? $subChild->name_cn : $subChild->name }}">
+                                                        {{ app()->getLocale()=='cn' && !empty($subChild->name_cn) ? $subChild->name_cn : $subChild->name }}
                                                     </a>
-                                                </li>
-                                            </ul>
-                                        </div>
+                                                    @endforeach
+                                                </div>
+                                            </div>
+                                        @else
+                                            @php
+                                                $menuUrl = $sub->getMenuUrl();
+                                                $pathwaySubmenu = strpos($menuUrl,'University-Pathway-Collection') !== false;
+                                            @endphp
+                                            <div class="aside">
+                                                <ul class="menu-list">
+                                                    <li>
+                                                        <a href="{{ $sub->link_to=='/' ? '/' : $sub->getMenuUrl() }}" title="{{ app()->getLocale()=='cn' && !empty($sub->name_cn) ? $sub->name_cn : $sub->name }}">
+                                                            {{ app()->getLocale()=='cn' && !empty($sub->name_cn) ? $sub->name_cn : $sub->name }}
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        @endif
                                     @endforeach
                                 </ul>
                             @endif
