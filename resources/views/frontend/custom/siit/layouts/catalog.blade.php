4bd79c9b1164031103b129ac4e470d44db758beb
slimerence <39113678+slimerence@users.noreply.github.com>
2018-10-25 11:38:20 +1100

    up

--- resources/views/frontend/custom/siit/layouts/catalog.blade.php
+++ resources/views/frontend/custom/siit/layouts/catalog.blade.php
@@ -3,11 +3,27 @@
 @include('layouts.frontend.head')
 <body style="{{ \Illuminate\Support\Facades\URL::current() == url('/') ? null : 'background-color: #f9f8f8;' }}">
 @if($agentObject->isPhone())
-    @include(_get_frontend_layout_path('frontend.header.general'))
-    @include( _get_frontend_layout_path('frontend.session_flash_msg_box'))
-    @yield('content')
-    @include( _get_frontend_layout_path('frontend.footer') )
-    @include( _get_frontend_layout_path('frontend.floating_box'))
+    @include('layouts.frontend.mobile.nav')
+    <main id="panel">
+        <header>
+            @include('layouts.frontend.mobile.header_mobile')
+        </header>
+        <section id="main">
+            @if( \Illuminate\Support\Facades\URL::current() == url('/') )
+                <div>
+                    <img src="{{ asset('/images/frontend/h1.png') }}" alt="a bridge across cultures">
+                </div>
+            @endif
+            <div class="container is-fluid">
+                @include('layouts.frontend.session_flash_msg_box')
+                <div id="panel">
+                    @yield('content')
+                </div>
+                @include( _get_frontend_layout_path('frontend.social_media_section') )
+                @include('layouts.frontend.footer')
+            </div>
+        </section>
+    </main>
 @else
     @include(_get_frontend_layout_path('frontend.header.general'))
     @include( _get_frontend_layout_path('frontend.session_flash_msg_box'))
